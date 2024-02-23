a7f20e9485987c02c4766a125766dfba8422e4a0
Louis Le Doussal <louis.le-doussal@mmibordeaux.com>
2024-02-23 10:24:21 +0100

    formulaire + tests

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,11 @@
 class Card < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :attack_name, presence: true
+  validates :damages, presence: true
+  validates :action, presence: true
+  validates :image_card, presence: true
+  validates :pv, presence: true
+
+
 end
